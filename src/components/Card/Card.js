ea9cc47e65a79e7904d8be0172bd6090144e3993
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-12-01 16:03:51 +0100

    ☄ creating card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import * as react from 'react';
+import { Link } from 'react-router-dom';
+import './Card.css';
+
+
+function Header(props) {
+    return (
+        <div className="card">
+            <Link to={props.link}>
+                <div className="card-year">
+                    <p>{props.year}</p>
+                </div>
+                <div className="card-image">
+                    <img src={props.image} alt={props.title} />
+                </div>
+                <div className="card-content">
+                    <h2 className="card-title">{props.title}</h2>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default Header;
