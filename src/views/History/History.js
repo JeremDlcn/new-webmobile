2be13071099949b775a9d0080216d1824b79cd9e
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-12-02 13:41:39 +0100

    🍱 import dynamicly assets

--- src/views/History/History.js
+++ src/views/History/History.js
@@ -2,37 +2,32 @@ import * as react from 'react';
 import './History.css';
 import Header from '../../components/Header/Header';
 import Card from '../../components/Card/Card';
-import logo from "../../../src/assets/years/2024/tokyo.png";
 
 //import all images from years folder
 const importAll = (r) => {
     return r.keys().map(r);
 }
+const images = importAll(require.context('../../../src/assets/years', true, /\.(png|jpe?g|svg)$/));
 
-console.log(importAll(require.context('../../../src/assets/years', true, /\.(png|jpe?g|svg)$/)));
 
 const events = [
     {
         year: "2012",
-        image: logo,
         title: "Londres",
         link: "/history/2012"
     },
     {
         year: "2016",
-        image: logo,
         title: "Rio",
         link: "/history/2016"
     },
     {
         year: "2020",
-        image: logo,
         title: "Tokyo",
         link: "/history/2020"
     },
     {
         year: "2024",
-        image: logo,
         title: "Paris",
         link: "/history/2024"
     },
@@ -42,7 +37,7 @@ const Events =  events.map((event) => {
     return (
         <Card
         year={event.year}
-        image={event.image}
+        image={images[events.indexOf(event)]}
         title={event.title}
         link={event.link}
         key={event.year.toString()}
