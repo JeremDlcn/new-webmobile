a3ecdc402743dfd084a609f7f7ce9ec40b24283b
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-12-02 16:28:53 +0100

    🎏 Styling Smallcard component

--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -7,7 +7,7 @@ const importAll = (r) => {
     return r.keys().map(r);
 }
 const images = importAll(require.context('../../../src/assets/years', true, /\.(png|jpe?g|svg)$/));
-const img = images.filter((img) => {return img.includes("logo");}); //get logos
+const img = images.filter((img) => { return img.includes("logo"); }); //get logos
 
 const Game = () => {
     let { id } = useParams();
@@ -15,11 +15,14 @@ const Game = () => {
     return (
         <div className="game">
             <Header />
-            <SmallCard
-                image={img[0]}
-                title={'Tokyo'}
-                year={id}
-            />
+            <div className="game-head">
+                <SmallCard
+                    image={img[0]}
+                    title={'Tokyo'}
+                    year={id}
+                />
+                
+            </div>
         </div>
     )
 }
