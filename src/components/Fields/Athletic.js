ac998f46fb9fe36cdee7340db41aecf3080d41f5
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-12-04 15:52:00 +0100

    🚧 better fields

--- src/components/Fields/Athletic.js
+++ src/components/Fields/Athletic.js
@@ -1,8 +1,37 @@
+import Flag from '../Flag/Flag';
+import Medals from '../Medals/Medals';
 import './Fields.css';
+
 const Athletic = () => {
     return (
-        <div className="athletic">
-            <p>Terrain d'Athlétisme</p>
+        <div className="gym">
+            <h1>Athlétisme</h1>
+            <ul className="field athletic-field">
+                <li className="field-item">
+                    <Flag country="France" />
+                    <Medals
+                        gold="1"
+                        silver="0"
+                        bronze="3"
+                    />
+                </li>
+                <li className="field-item">
+                    <Flag country="Japon" />
+                    <Medals
+                        gold="2"
+                        silver="1"
+                        bronze="0"
+                    />
+                </li>
+                <li className="field-item">
+                    <Flag country="Italie" />
+                    <Medals
+                        gold="1"
+                        silver="2"
+                        bronze="1"
+                    />
+                </li>
+            </ul>
         </div>
     );
 }
--- src/components/Fields/Pool.js
+++ src/components/Fields/Pool.js
@@ -1,8 +1,37 @@
+import Flag from '../Flag/Flag';
+import Medals from '../Medals/Medals';
 import './Fields.css';
+
 const Pool = () => {
     return (
-        <div className="pool">
-            <p>Piscine Olympique</p>
+        <div className="gym">
+            <h1>Natation</h1>
+            <ul className="field pool-field">
+                <li className="field-item">
+                    <Flag country="France" />
+                    <Medals
+                        gold="1"
+                        silver="0"
+                        bronze="3"
+                    />
+                </li>
+                <li className="field-item">
+                    <Flag country="Japon" />
+                    <Medals
+                        gold="2"
+                        silver="1"
+                        bronze="0"
+                    />
+                </li>
+                <li className="field-item">
+                    <Flag country="Italie" />
+                    <Medals
+                        gold="1"
+                        silver="2"
+                        bronze="1"
+                    />
+                </li>
+            </ul>
         </div>
     );
 }
