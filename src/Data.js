4dc584514b5ef53b8f3e4dbe85c32c91091d601a
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-12-11 23:19:18 +0100

    📦 adding data

--- src/Data.js
+++ src/Data.js
@@ -6,62 +6,62 @@ const data = [
         game: {
             athletics: [
                 {
-                    country: 'France',
-                    gold: 2,
-                    silver: 1,
-                    bronze: 1
+                    country: 'USA',
+                    gold: 13,
+                    silver: 7,
+                    bronze: 6
                 },
                 {
-                    country: 'Japon',
-                    gold: 1,
-                    silver: 1,
-                    bronze: 1
+                    country: 'URSS',
+                    gold: 10,
+                    silver: 6,
+                    bronze: 10
                 },
                 {
-                    country: 'Italie',
-                    gold: 1,
-                    silver: 1,
-                    bronze: 0
+                    country: 'Allemagne',
+                    gold: 6,
+                    silver: 11,
+                    bronze: 10
                 }
             ],
             swimming: [
                 {
-                    country: 'Japon',
-                    gold: 2,
-                    silver: 1,
-                    bronze: 1
+                    country: 'Allemagne',
+                    gold: 11,
+                    silver: 8,
+                    bronze: 9
                 },
                 {
-                    country: 'Italie',
-                    gold: 1,
-                    silver: 1,
-                    bronze: 1
+                    country: 'USA',
+                    gold: 8,
+                    silver: 6,
+                    bronze: 4
                 },
                 {
-                    country: 'France',
-                    gold: 1,
-                    silver: 0,
+                    country: 'Hongrie',
+                    gold: 4,
+                    silver: 2,
                     bronze: 0
                 }
             ],
             gym: [
                 {
-                    country: 'France',
-                    gold: 2,
-                    silver: 1,
-                    bronze: 1
+                    country: 'URSS',
+                    gold: 5,
+                    silver: 2,
+                    bronze: 0
                 },
                 {
-                    country: 'Italie',
-                    gold: 1,
-                    silver: 1,
+                    country: 'Allemagne',
+                    gold: 3,
+                    silver: 2,
                     bronze: 1
                 },
                 {
                     country: 'Japon',
-                    gold: 1,
-                    silver: 0,
-                    bronze: 0
+                    gold: 2,
+                    silver: 1,
+                    bronze: 1
                 }
             ],
         }
@@ -69,47 +69,605 @@ const data = [
     {
         year: "1992",
         title: "Barcelone",
-        link: "/game/1992"
+        link: "/game/1992",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 12,
+                    silver: 8,
+                    bronze: 10
+                },
+                {
+                    country: 'JO',
+                    gold: 7,
+                    silver: 11,
+                    bronze: 3
+                },
+                {
+                    country: 'Allemagne',
+                    gold: 4,
+                    silver: 1,
+                    bronze: 5
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 11,
+                    silver: 9,
+                    bronze: 7
+                },
+                {
+                    country: 'JO',
+                    gold: 6,
+                    silver: 3,
+                    bronze: 1
+                },
+                {
+                    country: 'Hongrie',
+                    gold: 5,
+                    silver: 3,
+                    bronze: 1
+                }
+            ],
+            gym: [
+                {
+                    country: 'Chine',
+                    gold: 9,
+                    silver: 1,
+                    bronze: 4
+                },
+                {
+                    country: 'USA',
+                    gold: 2,
+                    silver: 6,
+                    bronze: 2
+                },
+                {
+                    country: 'Roumanie',
+                    gold: 1,
+                    silver: 0,
+                    bronze: 1
+                }
+            ],
+        }
     },
     {
         year: "1996",
         title: "Atlanta",
-        link: "/game/1996"
+        link: "/game/1996",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 13,
+                    silver: 5,
+                    bronze: 6
+                },
+                {
+                    country: 'Russie',
+                    gold: 3,
+                    silver: 6,
+                    bronze: 1
+                },
+                {
+                    country: 'Allemagne',
+                    gold: 3,
+                    silver: 1,
+                    bronze: 3
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 13,
+                    silver: 11,
+                    bronze: 2
+                },
+                {
+                    country: 'Russie',
+                    gold: 4,
+                    silver: 2,
+                    bronze: 2
+                },
+                {
+                    country: 'Hongrie',
+                    gold: 3,
+                    silver: 1,
+                    bronze: 2
+                }
+            ],
+            gym: [
+                {
+                    country: 'Chine',
+                    gold: 9,
+                    silver: 1,
+                    bronze: 4
+                },
+                {
+                    country: 'USA',
+                    gold: 2,
+                    silver: 6,
+                    bronze: 2
+                },
+                {
+                    country: 'Roumanie',
+                    gold: 1,
+                    silver: 0,
+                    bronze: 1
+                }
+            ],
+        }
     },
     {
         year: "2000",
         title: "Sydney",
-        link: "/game/2000"
+        link: "/game/2000",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 7,
+                    silver: 4,
+                    bronze: 5
+                },
+                {
+                    country: 'Ethiopie',
+                    gold: 4,
+                    silver: 1,
+                    bronze: 5
+                },
+                {
+                    country: 'Pologne',
+                    gold: 4,
+                    silver: 0,
+                    bronze: 0
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 14,
+                    silver: 8,
+                    bronze: 11
+                },
+                {
+                    country: 'Australie',
+                    gold: 5,
+                    silver: 9,
+                    bronze: 4
+                },
+                {
+                    country: 'Pays-Bas',
+                    gold: 5,
+                    silver: 1,
+                    bronze: 2
+                }
+            ],
+            gym: [
+                {
+                    country: 'Chine',
+                    gold: 9,
+                    silver: 1,
+                    bronze: 4
+                },
+                {
+                    country: 'USA',
+                    gold: 2,
+                    silver: 6,
+                    bronze: 2
+                },
+                {
+                    country: 'Roumanie',
+                    gold: 1,
+                    silver: 0,
+                    bronze: 1
+                }
+            ],
+        }
     },
     {
         year: "2004",
         title: "Athènes",
-        link: "/game/2004"
+        link: "/game/2004",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 9,
+                    silver: 11,
+                    bronze: 5
+                },
+                {
+                    country: 'Russie',
+                    gold: 6,
+                    silver: 7,
+                    bronze: 6
+                },
+                {
+                    country: 'UK',
+                    gold: 3,
+                    silver: 0,
+                    bronze: 1
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 12,
+                    silver: 9,
+                    bronze: 10
+                },
+                {
+                    country: 'Australie',
+                    gold: 8,
+                    silver: 6,
+                    bronze: 7
+                },
+                {
+                    country: 'Chine',
+                    gold: 7,
+                    silver: 3,
+                    bronze: 1
+                }
+            ],
+            gym: [
+                {
+                    country: 'Chine',
+                    gold: 9,
+                    silver: 1,
+                    bronze: 4
+                },
+                {
+                    country: 'USA',
+                    gold: 2,
+                    silver: 6,
+                    bronze: 2
+                },
+                {
+                    country: 'Roumanie',
+                    gold: 1,
+                    silver: 0,
+                    bronze: 1
+                }
+            ],
+        }
     },
     {
         year: "2008",
         title: "Bejing",
-        link: "/game/2008"
+        link: "/game/2008",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 7,
+                    silver: 9,
+                    bronze: 7
+                },
+                {
+                    country: 'Kenya',
+                    gold: 6,
+                    silver: 4,
+                    bronze: 4
+                },
+                {
+                    country: 'Jamaique',
+                    gold: 6,
+                    silver: 3,
+                    bronze: 2
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 12,
+                    silver: 9,
+                    bronze: 10
+                },
+                {
+                    country: 'Australie',
+                    gold: 6,
+                    silver: 6,
+                    bronze: 8
+                },
+                {
+                    country: 'UK',
+                    gold: 2,
+                    silver: 2,
+                    bronze: 2
+                }
+            ],
+            gym: [
+                {
+                    country: 'Chine',
+                    gold: 9,
+                    silver: 1,
+                    bronze: 4
+                },
+                {
+                    country: 'USA',
+                    gold: 2,
+                    silver: 6,
+                    bronze: 2
+                },
+                {
+                    country: 'Roumanie',
+                    gold: 1,
+                    silver: 0,
+                    bronze: 1
+                }
+            ],
+        }
     },
     {
         year: "2012",
         title: "Londres",
-        link: "/game/2012"
+        link: "/game/2012",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 9,
+                    silver: 12,
+                    bronze: 7
+                },
+                {
+                    country: 'Jamaique',
+                    gold: 4,
+                    silver: 5,
+                    bronze: 3
+                },
+                {
+                    country: 'Russie',
+                    gold: 4,
+                    silver: 3,
+                    bronze: 3
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 16,
+                    silver: 9,
+                    bronze: 6
+                },
+                {
+                    country: 'Chine',
+                    gold: 5,
+                    silver: 2,
+                    bronze: 3
+                },
+                {
+                    country: 'France',
+                    gold: 4,
+                    silver: 2,
+                    bronze: 1
+                }
+            ],
+            gym: [
+                {
+                    country: 'Chine',
+                    gold: 4,
+                    silver: 3,
+                    bronze: 1
+                },
+                {
+                    country: 'USA',
+                    gold: 3,
+                    silver: 1,
+                    bronze: 2
+                },
+                {
+                    country: 'Russie',
+                    gold: 1,
+                    silver: 3,
+                    bronze: 4
+                }
+            ],
+        }
     },
     {
         year: "2016",
         title: "Rio",
-        link: "/game/2016"
+        link: "/game/2016",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 13,
+                    silver: 10,
+                    bronze: 9
+                },
+                {
+                    country: 'Kenya',
+                    gold: 6,
+                    silver: 6,
+                    bronze: 1
+                },
+                {
+                    country: 'Jamaique',
+                    gold: 6,
+                    silver: 3,
+                    bronze: 2
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 16,
+                    silver: 8,
+                    bronze: 9
+                },
+                {
+                    country: 'Australie',
+                    gold: 3,
+                    silver: 4,
+                    bronze: 3
+                },
+                {
+                    country: 'Hongrie',
+                    gold: 3,
+                    silver: 2,
+                    bronze: 2
+                }
+            ],
+            gym: [
+                {
+                    country: 'USA',
+                    gold: 4,
+                    silver: 6,
+                    bronze: 2
+                },
+                {
+                    country: 'UK',
+                    gold: 2,
+                    silver: 1,
+                    bronze: 3
+                },
+                {
+                    country: 'Japon',
+                    gold: 2,
+                    silver: 0,
+                    bronze: 1
+                }
+            ],
+        }
     },
     {
         year: "2020",
         title: "Tokyo",
-        link: "/game/2020"
+        link: "/game/2020",
+        game: {
+            athletics: [
+                {
+                    country: 'USA',
+                    gold: 7,
+                    silver: 12,
+                    bronze: 7
+                },
+                {
+                    country: 'Italie',
+                    gold: 5,
+                    silver: 0,
+                    bronze: 0
+                },
+                {
+                    country: 'Kenya',
+                    gold: 4,
+                    silver: 4,
+                    bronze: 2
+                }
+            ],
+            swimming: [
+                {
+                    country: 'USA',
+                    gold: 11,
+                    silver: 10,
+                    bronze: 9
+                },
+                {
+                    country: 'Australie',
+                    gold: 9,
+                    silver: 3,
+                    bronze: 9
+                },
+                {
+                    country: 'UK',
+                    gold: 4,
+                    silver: 3,
+                    bronze: 1
+                }
+            ],
+            gym: [
+                {
+                    country: 'Chine',
+                    gold: 3,
+                    silver: 3,
+                    bronze: 2
+                },
+                {
+                    country: 'Russie',
+                    gold: 2,
+                    silver: 2,
+                    bronze: 4
+                },
+                {
+                    country: 'USA',
+                    gold: 2,
+                    silver: 2,
+                    bronze: 2
+                }
+            ],
+        }
     },
     {
         year: "2024",
         title: "Paris",
-        link: "/game/2024"
+        link: "/game/2024",
+        game: {
+            athletics: [
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                },
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                },
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                }
+            ],
+            swimming: [
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                },
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                },
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                }
+            ],
+            gym: [
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                },
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                },
+                {
+                    country: 'France',
+                    gold: 0,
+                    silver: 0,
+                    bronze: 0
+                }
+            ],
+        }
     },
 ];
 
